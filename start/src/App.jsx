e66d14faf688a20a4d911f560f37413f20cb0d63
HekmatAbduweli <143352767+HekmatAbduweli@users.noreply.github.com>
2024-02-06 12:34:08 +0100

    react worwkshop

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -1,48 +1,25 @@
 import { useState } from "react";
 import "./App.css";
-import { products } from "./assets/legodudes";
-import Title from "./conmponents/Title";
-import ProductCard from "./conmponents/ProcductCard";
-import Header from "./conmponents/Header";
+import Header from "./components/Header";
+import ContentPage from "./components/ContentPage";
+import Nav from "./components/Nav";
 
 function App() {
-
-  console.log(products);
+  const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState("Ninjago");
+  const [cart, setCart] = useState([]);
 
   return (
     <div id="container">
-      <Header />
-      <nav>
-        <ul>
-          <li>
-            <a href="#">City</a>
-          </li>
-          <li>
-            <a href="#">Ninjago</a>
-          </li>
-          <li>
-            <a href="#">Castles and Knights</a>
-          </li>
-          <li>
-            <a href="#">Marine and Pirates</a>
-          </li>
-          <li>
-            <a href="#">Movie Characters</a>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <Title />
-        {products.map((product) => (
-          <ProductCard
-            key={product.prodid}
-            category={product.category}
-            title={product.title}
-            img={product.imagefile}
-            price={product.price}
-          />
-        ))}
-      </main>
+      <Header amount={amount} cart={cart} setCart={setCart} />
+      <Nav category={category} />
+      <ContentPage
+        amount={amount}
+        setAmount={setAmount}
+        category={category}
+        setCart={setCart}
+        cart={cart}
+      />
     </div>
   );
 }
---
+++ start/src/conmponents/Nav.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Nav({ category }) {
+  const categoryList = [
+    "City",
+    "Ninjago",
+    "Castles and Knights",
+    "Marine and Pirates",
+    "Movie Characters",
+  ];
+  return (
+    <nav>
+      <ul>
+        {categoryList.map((item, i) => (
+          <li key={i + "cat"}>
+            <a className={category === item ? "active" : null} href="#">
+              {item}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
